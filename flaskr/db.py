5399b7a731360b8223bb10a1c7bdd8b2d7a01f61
Kevin Warui <waruimacharia@gmail.com>
2023-12-05 12:26:31 +0300

    Add database initialization logic to flaskr/db.py

--- flaskr/db.py
+++ flaskr/db.py
@@ -20,3 +20,14 @@ def close_db(e=None):
     db = g.pop("db", None)
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf-8'))
+
+@click.command('init-db')
+def init_db_command():
+    """Clear existing data and create new tables"""
+    init_db()
+    click.echo('Initialized the database')
